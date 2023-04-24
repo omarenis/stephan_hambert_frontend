44e7288d226a388aa0157dc87b11cedec798b5d9
omar triki <omar.triki@enis.tn>
2023-04-24 08:10:17 +0100

    trying to fix the problem

---
+++ src/app/screens/public/ecommerce/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {Product} from "../../../../models/Product";
+import {AbstractRestService} from "../../../../services/genericservice";
+import {environment} from "../../../../../environments/environment";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit{
+  products !:Product[];
+  private actionUrl = `${environment.url}/products`
+  constructor(private productService: AbstractRestService<Product>){
+
+  }
+
+  ngOnInit(): void {
+    this.productService.list(this.actionUrl).subscribe(products => {
+      this.products = products;
+    console.log(this.products);
+    });
+  }
+
+}
