e231e340b43297b78034989abdc3f2e30be9c60f
Omar TRIKI <omartriki@stratisworldwide.com>
2023-05-25 17:04:19 +0100

    adding tables

--- src/app/models/Collection.ts
+++ src/app/models/Collection.ts
@@ -1,17 +1,5 @@
-import {Statistics} from "./Statistics";
-
-export class Collection extends Statistics {
-
-
-  constructor(
-    public image: string | File,
-    public label: string,
-    public description: string,
-    public override number_products: number,
-    public override number_purchases: number,
-    public override total_gain: number,
-    public override id ?: number,
-  ) {
-    super(number_products, number_purchases, total_gain, id)
-  }
+export interface Collection {
+    image: string | File;
+    label: string;
+    description: string;
 }
