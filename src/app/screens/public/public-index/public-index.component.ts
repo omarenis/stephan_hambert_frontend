32cc1db25925e5abb45e634bc389747464d1c0d3
omar triki <omar.triki@enis.tn>
2023-06-21 08:14:56 +0100

    adding the animations to the project

--- src/app/screens/public/public-index/public-index.component.ts
+++ src/app/screens/public/public-index/public-index.component.ts
@@ -2,17 +2,44 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {Collection} from "../../../models/Collection";
 import {AbstractRestService} from "../../../services/genericservice";
 import {DOCUMENT} from "@angular/common";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-public-index',
   templateUrl: './public-index.component.html',
-  styleUrls: ['./public-index.component.css']
+  styleUrls: ['./public-index.component.css'],
+  animations: [
+    trigger('left', [
+      state('true', style({
+        transform: 'translate(-100%)',
+      })),
+      state('false', style({
+        transform: 'translate(100%)'
+      })),
+      transition('true => false', animate('1.5s ease-in-out')),  // animation timing
+      transition('false => true', animate('1.5s ease-in-out')),
+      transition('true => true', animate('1.5s ease-in-out')),
+      transition('false => false', animate('1.5s ease-in-out'))
+    ]),
+    trigger('right', [
+      state('true', style({
+        transform: 'translate(100%)',
+      })),
+      state('false', style({
+        transform: 'translate(-100%)'
+      })),
+      transition('true => false', animate('1000ms linear')),  // animation timing
+      transition('false => true', animate('1000ms linear'))
+    ])
+  ]
 })
 export class PublicIndexComponent implements OnInit {
   step !: number;
   collections !: Collection[];
   startItem !: number;
-
+  move !: string;
+  left !: string;
+  right !: string;
   ngOnInit(): void {
     this.step = 0;
     this.startItem = 0;
@@ -23,11 +50,13 @@ export class PublicIndexComponent implements OnInit {
     document.documentElement.setAttribute('style', 'overflow-x: hidden');
   }
 
-  nextSlider() {
-    this.startItem++;
+  prevSlider() {
+    this.left = 'true';
+    this.right = '';
   }
 
-  addStep() {
-    this.startItem++
+  nextSlider() {
+    this.right = 'true';
+    this.left = '';
   }
 }
