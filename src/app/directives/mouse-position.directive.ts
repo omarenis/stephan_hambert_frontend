6605c9157f5182899c5d2c3cb22595b42deb8564
Omar TRIKI <omartriki@stratisworldwide.com>
2023-06-26 16:33:10 +0100

    adding the arrow function

---
+++ src/app/directives/mouse-position.directive.ts
@@ -0,0 +1,24 @@
+import {Directive, ElementRef, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appMousePosition]'
+})
+export class MousePositionDirective {
+
+  constructor(private element: ElementRef) {
+  }
+
+  @HostListener('mouseenter', ['$event'])
+  async onMouseMove(event: any) {
+    if (!this.element.nativeElement.parentNode.classList.contains('center')) {
+      if (event.target.id > 2) {
+        this.element.nativeElement.classList.remove("right");
+        this.element.nativeElement.classList.add('left');
+      } else {
+                this.element.nativeElement.classList.remove("left");
+        this.element.nativeElement.classList.add('right');
+      }
+      return;
+    }
+  }
+}
