63888a489f6a4b1d3f7b99dd3e22b77bc8a177e8
Omar TRIKI <omartriki@stratisworldwide.com>
2023-07-04 13:30:04 +0100

    adding the newsletter

--- src/app/screens/public/components/newsletter/newsletter.component.ts
+++ src/app/screens/public/components/newsletter/newsletter.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 interface Inscription {
   email: string;
@@ -12,9 +13,13 @@ interface Inscription {
 
 
 export class NewsletterComponent implements OnInit {
+  formGroup !: FormGroup;
   constructor() {
   }
   ngOnInit() {
 
+    this.formGroup = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    })
   }
 }
