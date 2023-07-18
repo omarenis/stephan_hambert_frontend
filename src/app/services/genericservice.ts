e7e5e763fdd51946bf6c820fb1681ec4ce126478
omar triki <omar.triki@enis.tn>
2023-07-18 08:20:34 +0100

    solving the problem of the project

--- src/app/services/genericservice.ts
+++ src/app/services/genericservice.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
-import Swal from 'sweetalert2';
+
 
 @Injectable({
   providedIn: 'root'
